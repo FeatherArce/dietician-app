9036050f0346f69c81014b05fdc8fa05c5d42965
Arce <ice810406@gmail.com>
2025-10-27 17:17:53 +0800

    刪除不再使用的 auth-fetch 和 formatter 工具

--- src/utils/formatter.ts
+++
@@ -1,8 +0,0 @@
-// 格式化金額
-export function formatCurrency(amount: number) {
-    return new Intl.NumberFormat('zh-TW', {
-        style: 'currency',
-        currency: 'TWD',
-        minimumFractionDigits: 0
-    }).format(amount);
-};
