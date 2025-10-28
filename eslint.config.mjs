fed31e3ef08d8ab667e38a14bba2794050979b7b
Arce <ice810406@gmail.com>
2025-10-28 16:45:44 +0800

    feat: 更新 ESLint 配置，調整目錄解析及忽略規則

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,34 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import reactHooks from 'eslint-plugin-react-hooks';
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
+})
+
 
 const eslintConfig = [
+  // 使用 Next.js 的推薦規則
+  ...compat.config({
+    extends: ['next/typescript'],  // next/core-web-vitals 會出錯
+  }),
+  reactHooks.configs.flat.recommended,
+  {
+    plugins: { 'react-hooks': reactHooks },
+    rules: {
+      // 在 useCallback 和 useMemo 的依賴項中允許省略變量
+      "react-hooks/exhaustive-deps": 'warn',
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
   {
     ignores: [
       "node_modules/**",
       ".next/**",
-      "out/**",
-      "build/**",
       "next-env.d.ts",
-      // 忽略 Prisma 生成的文件
       "prisma-generated/**",
-      "**/prisma-generated/**",
     ],
   },
-  // 使用 Next.js 的推薦規則
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      // 在 useCallback 和 useMemo 的依賴項中允許省略變量
-      "react-hooks/exhaustive-deps": 'warn',
-    },
-  }
 ];
 
 export default eslintConfig;
