81ddd29bc4c0250ebd89fa7e5aaca89da07af150
Arce Feather <arcefeather@gmail.com>
2025-10-29 13:51:15 +0800

    feat: 新增 UpdateUser 函數以支援用戶資料的更新

---
+++ src/services/client/user.ts
@@ -0,0 +1,15 @@
+import { User } from "@/app/lunch/types";
+import { authFetch } from "@/libs/auth-fetch";
+
+export async function UpdateUser(userId: string, data: Partial<User>) {
+    const response = await authFetch(`/api/lunch/users/${userId}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+
+    const result = await response.json();
+    return { response, result };
+}
