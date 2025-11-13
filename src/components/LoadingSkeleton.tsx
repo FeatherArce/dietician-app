6f21b2b524dfd337120a817c8b3420d47f756ec3
Arce Feather <arcefeather@gmail.com>
2025-11-13 09:15:32 +0800

    feat: 新增 LoadingSkeleton 元件以改善載入狀態顯示

---
+++ src/components/LoadingSkeleton.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function LoadingSkeleton() {
+    return (
+        <div className='w-full h-full'>
+            <div className="skeleton w-full h-full" />
+        </div>
+    )
+}
