56d0483a7839200a6edd50a15797a6203d573fb6
Arce Feather <arcefeather@gmail.com>
2025-11-19 14:10:02 +0800

    feat: 新增 PageLink 組件以支援加載狀態顯示

---
+++ src/components/ui/PageLink.tsx
@@ -0,0 +1,23 @@
+'use client';
+import Link from 'next/link'
+import { useLinkStatus } from 'next/link'
+
+function Hint() {
+    const { pending } = useLinkStatus()
+    if (!pending) return null;
+    return (
+        <span className="loading loading-spinner" />
+    )
+}
+
+interface PageLinkProps extends React.ComponentProps<typeof Link> {
+    children?: React.ReactNode;
+}
+export default function PageLink({ title, children, ...props }: PageLinkProps) {
+    return (
+        <Link prefetch={false} {...props}>
+            <Hint />
+            {children}
+        </Link>
+    )
+}
