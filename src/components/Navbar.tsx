659b7059c10a43eaefcc044390fb8b0d4d65db7f
Arce Feather <arcefeather@gmail.com>
2025-11-21 09:38:42 +0800

    feat: 新增手機板和桌面板的功能選單與標題顯示

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -67,20 +67,24 @@ export default function Navbar() {
   return (
     <div className="navbar bg-base-100 border-b border-base-300">
       <div className="navbar-start">
+        {/* 手機板: 功能選單 Hamburger Menu */}
         <div className="dropdown md:hidden">
           <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
           </div>
           <NavbarMenu type="dropdown" items={menuItems} />
         </div>
+        {/* 桌面板: 標題 */}
         <div className="hidden md:block">
           <PageLink href="/" className="btn btn-ghost text-xl normal-case">訂餐管理系統</PageLink>
         </div>
       </div>
       <div className="navbar-center">
+        {/* 手機板: 標題 */}
         <div className="block md:hidden">
           <PageLink href="/" className="btn btn-ghost text-xl normal-case">訂餐管理系統</PageLink>
         </div>
+        {/* 桌面板: 功能選單 */}
         <div className="hidden md:block">
           <NavbarMenu type="menu" items={menuItems} />
         </div>
